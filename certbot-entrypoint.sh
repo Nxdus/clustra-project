988a56030cf60c0e689227bc7211ac611bfb2258
Nxdus <54182974+Nxdus@users.noreply.github.com>
2024-12-11 07:02:41 +0700

    Remove non-interactive and agree-tos flags from Certbot entrypoint script to simplify SSL certificate generation process

--- certbot-entrypoint.sh
+++ certbot-entrypoint.sh
@@ -9,8 +9,6 @@ if [ ! -f /etc/letsencrypt/live/clustra.tech/fullchain.pem ]; then
         --email paitong1550@gmail.com \
         --rsa-key-size 4096 \
         --keep-until-expiring \
-        --non-interactive \
-        --agree-tos \
         --preferred-challenges http-01 \
         --server https://acme-v02.api.letsencrypt.org/directory
 fi 
