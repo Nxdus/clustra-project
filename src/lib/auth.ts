8bcdc7a1427a8a2288e6d4590e06e87f6db8c070
Nxdus <54182974+Nxdus@users.noreply.github.com>
2024-12-22 22:54:59 +0700

    Enhance auth configuration by adding maxAge to cookie settings and simplifying callback logic. This improves session management and maintains secure cookie attributes.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -41,6 +41,7 @@ export const authOptions: AuthOptions = {
         sameSite: 'lax',
         httpOnly: true,
         secure: true,
+        maxAge: 0,
       },
     },
   },
@@ -71,15 +72,11 @@ export const authOptions: AuthOptions = {
   ],
   callbacks: {
     jwt: async ({ token, user }) => {
-      if (user) {
-        token.id = user.id
-      }
+      if (user) token.id = user.id
       return token
     },
     session: async ({ session, token }) => {
-      if (session?.user) {
-        session.user.id = token.id as string
-      }
+      if (session.user) session.user.id = token.id as string
       return session
     }
   },
